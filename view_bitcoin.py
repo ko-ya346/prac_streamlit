c83184be6d20808d3367a97dbf3340507868ce8b
kouya takahashi <3kamarin4skaya6@gmail.com>
2022-07-23 21:37:17 +0900

    [add] st.cache -> load_data

--- view_bitcoin.py
+++ view_bitcoin.py
@@ -1,13 +1,25 @@
 import streamlit as st
 import pandas as pd
 
-key = st.secrets.GoogleDriveApiKey.key
-print(key)
+
+# google drive上のcsvファイルのURL
 URL = (
     "https://drive.google.com/file/d/1Q2c6pRbZp16tzGUFLB9X-I_7IJcROxUW/view?usp=sharing"
 )
-path = f"https://www.googleapis.com/drive/v3/files/{URL.split('/')[-2]}?alt=media&key={key}"
+# google drive api key
+KEY = st.secrets.GoogleDriveApiKey.key
+
+@st.cache
+def load_data():
+    """
+    Args:
+        url: google drive 
+    """
+    path = f"https://www.googleapis.com/drive/v3/files/{URL.split('/')[-2]}?alt=media&key={KEY}"
+    return pd.read_csv(path)
+    
+
+df = load_data() 
 
 
-df = pd.read_csv(path)
 st.write(df.head(5))
